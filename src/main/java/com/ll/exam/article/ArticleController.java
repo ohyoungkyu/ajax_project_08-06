ea6fe3f7eef83be0a62d03464001298131219b48
ohyoungkyu <come8908@gmail.com>
2022-08-22 20:41:48 +0900

    게시물 리스트에서 등록된 게시물들이 나ì나오도록

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -9,35 +9,23 @@ import java.util.List;
 public class ArticleController {
 
     private ArticleService articleService;
-
     public ArticleController() {
         articleService = new ArticleService();
     }
 
     public void showList(Rq rq) {
-        List<ArticleDto> articleDtos = new ArrayList<>();
-        articleDtos.add(new ArticleDto(5,"제목5","내용5"));
-        articleDtos.add(new ArticleDto(4,"제목4","내용4"));
-        articleDtos.add(new ArticleDto(3,"제목3","내용3"));
-        articleDtos.add(new ArticleDto(2,"제목2","내용2"));
-        articleDtos.add(new ArticleDto(1,"제목1","내용1"));
+        List<ArticleDto> articleDtos = articleService.findAll();
 
-        rq.setAttr("articles",articleDtos);
-
+        rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
     }
-
     public void showWrite(Rq rq) {
         rq.view("usr/article/write");
     }
-
     public void doWrite(Rq rq) {
         String title = rq.getParam("title", "");
         String body = rq.getParam("body", "");
-
         long id = articleService.write(title, body);
-
         rq.appendBody("%d번 게시물이 생성 되었습니다.".formatted(id));
-
     }
 }
--- src/main/webapp/jsp/usr/article/write.jsp
+++ src/main/webapp/jsp/usr/article/write.jsp
@@ -27,12 +27,14 @@ function ArticleSave__submitForm(form) {
             <input name="title" type="text" maxlength="50" placeholder="제목을 입력해주세요." />
         </div>
     </div>
+
     <div>
         <span>내용</span>
         <div>
             <input name="body" type="text" maxlength="300" placeholder="내용을 입력해주세요." />
         </div>
     </div>
+
     <div>
         <span>작성</span>
         <div>
